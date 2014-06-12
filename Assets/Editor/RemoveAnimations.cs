09f56289cdd00378b414d6485ba72793b15d9f77
090317 <090317@nhtv.nl>
2014-06-12 20:38:39 +0200

    Remove ALL the unused animations!

--- Assets/Editor/RemoveAnimations.cs
+++ Assets/Editor/RemoveAnimations.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class RemoveAnimations : Editor {
 	
-	[MenuItem("WFH/RemoveAnimations")]
+	[MenuItem("WFH/RemoveAnimations %#g")]
 	static void fix(){
 		if (Selection.activeGameObject != null) {
 			Animation[] animationArray = Selection.activeGameObject.GetComponentsInChildren<Animation>();
