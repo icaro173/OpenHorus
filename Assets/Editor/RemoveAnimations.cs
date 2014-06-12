f8d7d57a7125793714c4a11a5e8901fb5b8ee4e9
090317 <090317@nhtv.nl>
2014-06-12 20:06:58 +0200

    Fixing animations on prefabs

---
+++ Assets/Editor/RemoveAnimations.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RemoveAnimations : Editor {
+	
+	[MenuItem("WFH/RemoveAnimations")]
+	static void fix(){
+		if (Selection.activeGameObject != null) {
+			Animation[] animationArray = Selection.activeGameObject.GetComponentsInChildren<Animation>();
+			foreach (Animation anim in animationArray) {
+				GameObject.DestroyImmediate(anim);
+			}
+		}
+	}
+}
