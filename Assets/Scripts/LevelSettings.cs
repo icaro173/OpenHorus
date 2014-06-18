81019d2b19c340bdf2c3cf12052303c3fcb547f2
Hamcha <zikyky@gmail.com>
2014-06-18 14:45:12 +0200

    Level dependant settings are a go!

---
+++ Assets/Scripts/LevelSettings.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LevelSettings : MonoBehaviour {
+    // Instance
+    public static LevelSettings instance;
+
+    // Level properties
+    public Transform spectatorCameraPosition { get { return transform; } }
+    public float killZ;
+
+    void Awake() {
+        instance = this;
+    }
+
+    void OnDestroy() {
+        if (instance == this)
+            instance = null;
+    }
+
+    void Start() {
+        // Set orbiting camera position
+        if (ServerScript.Instance != null) {
+            GameObject orbit = FindObjectOfType<CameraSpin>().gameObject;
+            orbit.transform.position = spectatorCameraPosition.position;
+            orbit.transform.rotation = spectatorCameraPosition.rotation;
+        }
+
+        // Are we starting this level directly? Do QuickPlay
+        //if (Application.isEditor && ServerScript.Instance == null) {
+            //todo Quick play here
+        //}
+    }
+
+}
